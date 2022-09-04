3ad5f49c2a1c6c55001e18b5ae8b1d16f8ecfebf
Balint Pecz <balint.pecz@evocrm.hu>
2022-09-04 22:38:14 +0200

    Done.

--- 08-other/exercises/03/solution/js/objectConverter.js
+++ 08-other/exercises/03/solution/js/objectConverter.js
@@ -1,1 +1,29 @@
+const objectConverter = {
+    arrayToMap(array) {
+      return new Map(array.map((item, index) => [index, item]));
+    },
+    arrayToSet(array) {
+         return new Set(array);
+    },
+    setToMap(array) {
+    return new Map(Array.from(array).map((item, index) => [index, item]));
+    },
+    setToArray(array) {
+    return [...array];
+    },
+    mapToArray(array) {
+        const temp = [];
+        for (const value of array) {
+          temp.push(value[1]);
+        }
+        return temp;
+    },
+    mapToSet(array) {
+        const setArray = [];
+        array.forEach(value => setArray.push(value));
+        return new Set(setArray);
+    },
+}
+
 export default objectConverter;
+
--- 08-other/exercises/04/solution/js/customDateFormats.js
+++ 08-other/exercises/04/solution/js/customDateFormats.js
@@ -1,1 +1,23 @@
+const customDateFormats = (date) => {
+    const short = date.toLocaleDateString('hu-HU', {
+        year: 'numeric',
+        month: 'short',
+        day: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+    });
+    const long = date.toLocaleDateString('hu-HU', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+    });
+
+    return { short, long };
+}
+
+
+
 export default customDateFormats;
